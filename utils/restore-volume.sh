6176497f29cc189d222224f1f471c78aade406c3
Andrea Conti <andrea.conti@tutanota.com>
2022-09-12 01:58:55 +0200

    bug fix and add backup tools

---
+++ utils/restore-volume.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/env bash
+
+VOLUME_NAME=$1
+VOLUME_BACKUP_PATH=$2
+TMP_MOUNT_FROM="/volume_data_from"
+TMP_MOUNT_TO="/volume_data_to"
+
+docker run --rm \
+    --volume $VOLUME_NAME:$TMP_MOUNT_FROM \
+    --volume $(realpath $VOLUME_BACKUP_PATH | xargs dirname):$TMP_MOUNT_TO \
+    ubuntu \
+    tar xvf $TMP_MOUNT_TO/$(basename $VOLUME_BACKUP_PATH) -C $TMP_MOUNT_FROM --strip 1
+
